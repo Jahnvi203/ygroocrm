b46daa1e86c6261766726f936c96692ede484bb8
Jahnvi203 <85999744+Jahnvi203@users.noreply.github.com>
2023-09-15 08:23:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -725,6 +725,8 @@ def get_contact_comms(id):
             current_dt = current_dt.strftime('%Y-%m-%dT%H:%M:%SZ')
             contact = contacts_col.find_one({'Contact ID': id})
             print(contact)
+            print(get_comms(299086, "email", "modifiedAt", "desc", "active", contact['Email']))
+            print(get_comms(299086, "email", "modifiedAt", "desc", "closed", contact['Email']))
             active_response = get_comms(299086, "email", "modifiedAt", "desc", "active", contact['Email'])["_embedded"]["conversations"]
             closed_response = get_comms(299086, "email", "modifiedAt", "desc", "closed", contact['Email'])["_embedded"]["conversations"]
             active_response_rows_html = ""
